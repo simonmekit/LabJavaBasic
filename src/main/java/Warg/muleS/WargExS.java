687e3bf65ef2e4b044490c0b3f24afcdb803ba04
simon mekit <simonmekit@gmail.com>
2023-12-25 22:15:25 -0500

    Autotype conversion code

--- src/main/java/Warg/muleS/WargExS.java
+++
@@ -1,8 +0,0 @@
-package Warg.muleS;
-
-public class WargExS {
-    public static void main(String[] args) {
-        System.out.println("Hi Warg");
-        System.out.println("Hi Warg second");
-    }
-}
